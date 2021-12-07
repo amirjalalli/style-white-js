9fd5d5308ba97c8c43e858107b59cb1e196347c6
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-12-07 20:55:00 +0330

    set-attribute

---
+++ js.js
@@ -0,0 +1,13 @@
+let h1Elem = document.getElementById('text-center')
+
+h1Elem.setAttribute('align','center')
+h1Elem.setAttribute('id','h1text')
+
+console.log(h1Elem)
+
+
+// ulElem.setAttribute("align","center")
+
+// ulElem.setAttribute('class','ul-list')
+// console.log(ulElem.setAttribute('id','list-item'))
+
